9f503d756b6353a589e5bc73dd12ad3239fc724b
nguyentuan <dungtuann67@gmail.com>
2023-04-16 09:06:19 +0700

    update cart

--- header.js
+++ header.js
@@ -51,3 +51,20 @@ function hideNavbar(e) {
         console.log("Vua bam ra ngoai");
     }
 }
+
+const cartBtn = document.querySelector(".cart-header-btn")
+const cart = document.querySelector("#cart")
+const closeCartBtn = document.querySelector(".cart_close")
+const overlay = document.querySelector(".overlay")
+cartBtn.addEventListener("click", () => {
+    cart.classList.add("active")
+    overlay.classList.add("active")
+})
+closeCartBtn.addEventListener("click", () => {
+    cart.classList.remove("active")
+    overlay.classList.remove("active")
+})
+overlay.addEventListener("click", () => {
+    cart.classList.remove("active")
+    overlay.classList.remove("active")
+})
